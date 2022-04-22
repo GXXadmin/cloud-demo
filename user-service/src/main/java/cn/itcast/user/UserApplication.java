8b7412774ed5401e2ed950104a330f4e58e3a4ec
gxx <G000852@163.com>
2022-04-22 23:03:29 +0800

    git mast

--- user-service/src/main/java/cn/itcast/user/UserApplication.java
+++ user-service/src/main/java/cn/itcast/user/UserApplication.java
@@ -8,9 +8,7 @@ import org.mybatis.spring.annotation.MapperScan;
 @SpringBootApplication
 public class UserApplication {
     public static void main(String[] args) {
-        System.out.println("1111111");
-        System.out.println("1111111");
-        System.out.println("1111111");
+        System.out.println("456789");
         System.out.println("1111111");
         SpringApplication.run(UserApplication.class, args);
     }
